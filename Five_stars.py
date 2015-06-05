8311542b1755a81a55d0f2ab9cec09a8b516686e
Learning_Py <LiXiao-Py@users.noreply.github.com>
2015-06-05 15:34:26 +0800

    Create Five_stars.py

---
+++ Five_stars.py
@@ -0,0 +1,21 @@
+# -*- encoding:utf-8 -*-
+# Draw a 5-pointed star
+import turtle
+import time
+
+turtle.color("red")
+turtle.goto(0,0)
+
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.goto(144,0)
+
+turtle.write("Welcome to Python")
+time.sleep(3)
